bad7cfae1f160db144958d6e9134751990779804
yannik131 <yannik131@web.de>
2026-07-01 12:32:01 +0200

    Fixed lv classifier script

--- scripts/FL/classify_full_set.py
+++ scripts/FL/classify_full_set.py
@@ -15,15 +15,16 @@ def process_row(row):
     is_lv = has_lv_dynamics(df)
     return (row.p1, row.p2, row.p3, row.p4, row.p5, row.p6, int(is_lv))
 
-with ProcessPoolExecutor(max_workers=4) as pool:
-    try:
-        futures = [pool.submit(process_row, r) for r in rows_iter]
+if __name__ == "__main__":
+    with ProcessPoolExecutor(max_workers=4) as pool:
+        try:
+            futures = [pool.submit(process_row, r) for r in rows_iter]
 
-        for f in tqdm(as_completed(futures), total=len(futures)):
-            rows.append(f.result())
+            for f in tqdm(as_completed(futures), total=len(futures)):
+                rows.append(f.result())
 
-        df_all = pd.DataFrame(rows, columns=["p1", "p2", "p3", "p4", "p5", "p6", "is_lv"])
-        df_all.to_csv(DATASETS_DIR / "FL/lv_heat_map_full_df.csv", index=False)
-    except KeyboardInterrupt:
-        print("Shutting down pool..")
-        pool.shutdown(wait=True, cancel_futures=True)
+            df_all = pd.DataFrame(rows, columns=["p1", "p2", "p3", "p4", "p5", "p6", "is_lv"])
+            df_all.to_csv(DATASETS_DIR / "FL/lv_heat_map_full_df.csv", index=False)
+        except KeyboardInterrupt:
+            print("Shutting down pool..")
+            pool.shutdown(wait=True, cancel_futures=True)
