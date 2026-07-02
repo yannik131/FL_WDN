93b2eb85073d7c15557e24f70a7de3009e622acf
yannik131 <yannik131@web.de>
2026-07-02 14:11:42 +0200

    Updated full fl classify script

--- scripts/FL/classify_full_set.py
+++ scripts/FL/classify_full_set.py
@@ -4,15 +4,16 @@ from tqdm import tqdm
 from util.paths import DATASETS_DIR
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
-csv_dir = DATASETS_DIR / "FL/full_lv_set/"
-mapping = pd.read_csv(DATASETS_DIR / "FL/full_lv_set.csv")
+set_name = "full_lv_set_2"
+csv_dir = DATASETS_DIR / f"FL/{set_name/"
+mapping = pd.read_csv(DATASETS_DIR / f"FL/{set_name}.csv")
 
 rows_iter = list(mapping.itertuples(index=False, name=None))
 rows = []
 
 def process_row(row):
     filename, p1, p2, p3, p4, p5, p6, r = row
-    df = pd.read_csv(DATASETS_DIR / f"FL/full_lv_set/{filename}")
+    df = pd.read_csv(DATASETS_DIR / f"FL/{set_name}/{filename}")
     is_lv = has_lv_dynamics(df)
     return (p1, p2, p3, p4, p5, p6, int(is_lv))
 
@@ -25,7 +26,7 @@ if __name__ == "__main__":
                 rows.append(f.result())
 
             df_all = pd.DataFrame(rows, columns=["p1", "p2", "p3", "p4", "p5", "p6", "is_lv"])
-            df_all.to_csv(DATASETS_DIR / "FL/lv_heat_map_full_df.csv", index=False)
+            df_all.to_csv(DATASETS_DIR / "FL/lv_heat_map_full_2_df.csv", index=False)
         except KeyboardInterrupt:
             print("Shutting down pool..")
             pool.shutdown(wait=True, cancel_futures=True)
