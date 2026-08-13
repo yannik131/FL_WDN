1ec4f0d24cbf24fd881b9fd240dc6d14069be602
yannik131 <yannik131@web.de>
2026-08-13 08:01:14 +0200

    small head

--- scripts/WDN/train_full_gnn.py
+++ scripts/WDN/train_full_gnn.py
@@ -98,7 +98,7 @@ class ReactionDataset(Dataset):
         self.samples = []
         rollout_steps = 20
         logger.info(f"Reading {mapping_file}")
-        mapping = pd.read_csv(mapping_file)
+        mapping = pd.read_csv(mapping_file).head(2000)
 
         for row in tqdm(mapping.itertuples(index=False), total=len(mapping)):
             filename = row.filename
